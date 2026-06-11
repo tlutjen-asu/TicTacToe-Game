bbe73eb84465ba00454ec35f11f25f6726791c5e
Trevor <tlutejn@asu.edu>
2026-06-10 21:59:27 -0500

    Add a prompt that tells the user the game is not implemented yet

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -2,5 +2,6 @@ public class TicTacToe {
   public static void main(String[] args) {
     System.out.println("Welcome to the TicTacToe!");
     // TODO: Implement the game logic
+    System.out.println("Tell the developer to actually create the game!");
   }
 }
