e33362707e8f3a393038cc87def8690a52a6f4d2
Trevor <tlutejn@asu.edu>
2026-06-10 22:16:41 -0500

    Add board display and player status functionality
    - displayBoard method shows current game state with coordinates
    
    - getCurrentPlayer method returns active player symbol
    
    - Formatted output improves user experience
    
    - Supports debugging and game state verification

--- src/GameEngine.java
+++ src/GameEngine.java
@@ -67,3 +67,36 @@ public class GameEngine {
 
 }
 
+
+ 
+
+    public void displayBoard() {
+
+        System.out.println("Current Board:");
+
+        System.out.println("  1 2 3");
+
+        for (int i = 0; i < 3; i++) {
+
+            System.out.print((i + 1) + " ");
+
+            for (int j = 0; j < 3; j++) {
+
+                System.out.print(board[i][j] + " ");
+
+            }
+
+            System.out.println();
+
+        }
+
+    }
+
+ 
+
+    public char getCurrentPlayer() {
+
+        return currentPlayer;
+
+    }
+
