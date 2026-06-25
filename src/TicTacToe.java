3c2f495bd42da489abe39981e08a96426cc29af9
tlutjen-asu <tlutjen@asu.edu>
2026-06-24 22:19:43 -0500

    Update TicTacToe.java
    added a note about github being used to make this change to make it apparent.

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -2,7 +2,7 @@ public class TicTacToe {
   public static void main(String[] args) {
     System.out.println("Welcome to the TicTacToe!");
     // TODO: Implement the game logic
-    System.out.println("Tell the developer to actually create the game!"); // He's working on it!
+    System.out.println("Tell the developer to actually create the game!"); // He's working on it! - Added on GitHub
   }
 }
 // TODO: Add colored output for X and O
